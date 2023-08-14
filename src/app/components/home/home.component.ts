674a23390f9b162e9f3bf8325c1547582db1202f
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-08-13 23:29:55 -0500

    Update navbar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -21,6 +21,27 @@ export class HomeComponent implements OnInit {
         _this.magnetize(elem, e);
       });
     });
+
+    const listItem = document.querySelectorAll('.home-header li');
+    const menuBackdrop = document.querySelector('#menu-backdrop') as HTMLDivElement;
+
+    listItem.forEach((item) => {
+      item.addEventListener('mouseenter', ({ target} : any) => {
+        const { left, top, width, height } = item.getBoundingClientRect();
+        menuBackdrop.style.setProperty("--left", `${left}px`);
+        menuBackdrop.style.setProperty("--top", `${top}px`);
+        menuBackdrop.style.setProperty("--width", `${width}px`);
+        menuBackdrop.style.setProperty("--heigth", `${height}px`);
+        console.log(menuBackdrop.style.getPropertyValue("--width"))
+        menuBackdrop.style.visibility = 'visible';
+        menuBackdrop.style.opacity = '1';
+      })
+
+      item.addEventListener('mouseleave', () => {
+        menuBackdrop.style.visibility = 'hidden';
+        menuBackdrop.style.opacity = '0';
+      })
+    })
   }
 
   // Magentic Elements
