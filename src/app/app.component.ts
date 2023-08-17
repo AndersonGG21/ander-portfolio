3d965ef66f84e933bea2d6d5ea7576cee9f4b5d4
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-08-17 17:18:27 -0500

    Add custom cursors followers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    gsap.set('.cursor', {xPercent: -50, yPercent: -50});
+    let cursor = document.querySelector('.cursor') as HTMLDivElement;
+    let mouseX = 0;
+    let mouseY = 0;
+
+    window.addEventListener('mousemove', (e) => {
+      mouseX = e.clientX;
+      mouseY = e.clientY;
+      gsap.to(cursor, 0.5, { x: mouseX, y: mouseY });
+    })
+  }
+  title = 'Anderson Garces - Portofolio';
 }
