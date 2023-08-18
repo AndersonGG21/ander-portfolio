dd553d7c8e40de163e3b7d4303b14174781e8588
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-08-18 18:39:14 -0500

    Change component selector

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
+  selector: 'About',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
