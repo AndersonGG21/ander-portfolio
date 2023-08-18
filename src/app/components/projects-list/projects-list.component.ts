78e9fa9d8f353d2c7c0c6550a04697ef88a39ede
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-08-18 18:41:17 -0500

    Chnage cursor on mouse enter

--- src/app/components/projects-list/projects-list.component.ts
+++ src/app/components/projects-list/projects-list.component.ts
@@ -1,13 +1,9 @@
 import {
   Component,
-  ElementRef,
-  HostListener,
-  OnInit,
-  QueryList,
-  Renderer2,
-  ViewChildren,
+  OnInit
 } from '@angular/core';
 import gsap from 'gsap';
+import { Elastic } from 'gsap';
 
 @Component({
   selector: 'app-projects-list',
@@ -20,13 +16,16 @@ export class ProjectsListComponent implements OnInit {
 
   ngOnInit(): void {
     this.preview = document.querySelector('.preview') as HTMLDivElement;
-    const projects = document.querySelector(".projects") as HTMLDivElement;
+    const projects = document.querySelector('.projects') as HTMLDivElement;
     Array.from(projects.children).forEach((project) => {
-      project.addEventListener("mousemove", this.moveProject.bind(this));
-      project.addEventListener("mousemove", this.movePorjectImg.bind(null, project));
+      project.addEventListener('mousemove', this.moveProject.bind(this));
+      project.addEventListener(
+        'mousemove',
+        this.movePorjectImg.bind(null, project)
+      );
     });
 
-    window.addEventListener("mousemove", this.moveStuff.bind(this));
+    window.addEventListener('mousemove', this.moveStuff.bind(this));
   }
 
   moveProject(e: any) {
@@ -34,16 +33,17 @@ export class ProjectsListComponent implements OnInit {
       const previewRect = this.preview.getBoundingClientRect();
       const offsetX = previewRect.width / 2;
       const offsetY = previewRect.height / 2;
-      this.preview.style.left = e.clientX - offsetX + 'px'
-      this.preview.style.top = e.clientY - offsetY + window.scrollY + 'px'
+      this.preview.style.left = e.clientX - offsetX + 'px';
+      this.preview.style.top = e.clientY - offsetY + window.scrollY + 'px';
     }
+
+    this.handCursor(e);
   }
 
-  moveStuff(e : MouseEvent) {
+  moveStuff(e: MouseEvent) {
     const mouseInside = this.isMouseInsideContainer(e);
     if (this.isInside != mouseInside) {
       this.isInside = mouseInside;
-      console.log(this.isInside);
       if (this.isInside) {
         gsap.to(this.preview, 0.3, {
           scale: 1,
@@ -56,11 +56,12 @@ export class ProjectsListComponent implements OnInit {
     }
   }
 
-  movePorjectImg(project : any) {
+  movePorjectImg(project: any) {
     const bgPositions = {
       p1: '0 0',
-      p2: '0 50%',
-      p3: '0 100%',
+      p2: '0 33.33%',
+      p3: '0 66.66%',
+      p4: '0 100%',
     };
     const previewImg = document.querySelector('.preview-img');
     const projectId = project.id as keyof typeof bgPositions;
@@ -69,9 +70,8 @@ export class ProjectsListComponent implements OnInit {
     });
   }
 
-
-  isMouseInsideContainer(e : MouseEvent){
-    const projects = document.querySelector(".projects") as HTMLDivElement;
+  isMouseInsideContainer(e: MouseEvent) {
+    const projects = document.querySelector('.projects') as HTMLDivElement;
     const containerRect = projects.getBoundingClientRect();
     return (
       e.clientX >= containerRect.left &&
@@ -79,11 +79,40 @@ export class ProjectsListComponent implements OnInit {
       e.clientY >= containerRect.top &&
       e.clientY <= containerRect.bottom
     );
-  };
+  }
 
-  projectRedirect() {
-    alert("SIUUU");
+  projectRedirect(id: string) {
+    alert(id);
+  }
+
+  onMouseEnter() {
+    gsap.set('.cursor', {opacity: 0});
+    gsap.to('.hand', 1, {
+      opacity: 1,
+      top: '-20px',
+      left: '-20px',
+      rotate: 0,
+      ease: Elastic.easeOut.config(1, 0.3),
+    })
+  }
+
+  onMouseLeave() {
+    gsap.to('.hand', 1,{ 
+      opacity: 0,
+      top: '10px',
+      left: '40px',
+      rotate: 45,
+    });
+    gsap.set('.cursor', {opacity: 1});
+  }
+
+  handCursor(e : MouseEvent) {
+    let mouseX = e.clientX;
+    let mouseY = e.clientY;
+
+    gsap.to('.hand', 1, {
+      x: mouseX,
+      y: mouseY,
+    })
   }
 }
-  
-
