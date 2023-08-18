1dfc75a583d031a5594efb24ea9872d973dd91fe
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-08-18 18:38:23 -0500

    Initialize Lenis on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, OnInit } from '@angular/core';
 import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Lenis from '@studio-freight/lenis';
+import SplitTextJS from 'split-text-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
+
   ngOnInit(): void {
-    gsap.set('.cursor', {xPercent: -50, yPercent: -50});
+    gsap.set('.cursor', { xPercent: -50, yPercent: -50 });
     let cursor = document.querySelector('.cursor') as HTMLDivElement;
     let mouseX = 0;
     let mouseY = 0;
@@ -17,7 +21,19 @@ export class AppComponent implements OnInit{
       mouseX = e.clientX;
       mouseY = e.clientY;
       gsap.to(cursor, 0.5, { x: mouseX, y: mouseY });
-    })
+    });
+
+    const lenis = new Lenis();
+
+    lenis.on('scroll', (e: any) => {
+      // console.log(e);
+    });
+
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
   }
   title = 'Anderson Garces - Portofolio';
 }
