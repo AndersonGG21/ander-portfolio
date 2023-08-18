89d4fd61e5fa1c2403eed3786ce74a0644949fd4
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-08-18 18:40:36 -0500

    Add props

--- src/app/components/marquee/marquee.component.ts
+++ src/app/components/marquee/marquee.component.ts
@@ -1,26 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import gsap from 'gsap';
 
 @Component({
-  selector: 'app-marquee',
+  selector: 'MarqueeText',
   templateUrl: './marquee.component.html',
   styleUrls: ['./marquee.component.css'],
 })
 export class MarqueeComponent implements OnInit {
+  @Input() id : string = '';
+  @Input() text : string = '';
+  @Input() duration : number = 0;
+  @Input() fontSize : number = 0;
+  @Input() opacity : number = 0;
+
   ngOnInit(): void {
     let currentScroll = 0;
     let isScrollingDown = true;
+    let marquee = document.querySelector(`#${this.id}`) as HTMLElement
 
+    marquee.style.setProperty('--fontSize', `${this.fontSize}px`)
+    marquee.style.opacity = `${this.opacity}`;
     let tween = gsap
-      .to('.marquee__part', {
+      .to(`#${this.id} .marquee__part`, {
         xPercent: -100,
         repeat: -1,
-        duration: 40,
+        duration: this.duration,
         ease: 'linear',
       })
       .totalProgress(0.3);
 
-    gsap.set('.marquee__inner', { xPercent: 1 });
+    gsap.set(`#${this.id}`, { xPercent: 1 });
 
     window.addEventListener('scroll', function () {
       if (window.scrollY > currentScroll) {
