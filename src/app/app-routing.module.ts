2f95e7168417d984fda963453de9f81c3bdb4334
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-08-28 17:31:09 -0500

    Add route for projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ProjectPageComponent } from './components/project-page/project-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'work/:project', component: ProjectPageComponent}
 ];
 
 @NgModule({
