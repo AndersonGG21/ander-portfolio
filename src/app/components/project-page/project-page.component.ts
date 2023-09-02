9cc1ef58f8da56df72e8d0f0394f7f2fa849b17f
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-09-01 22:24:04 -0500

    Moves project logic to a service

--- src/app/components/project-page/project-page.component.ts
+++ src/app/components/project-page/project-page.component.ts
@@ -4,18 +4,9 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { Power2 } from 'gsap';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { MagnetizeElementsService } from 'src/app/services/magnetize-elements.service';
+import { Project, ProjectsDataService } from 'src/app/services/projects-data.service';
 
-interface Project {
-  id: string;
-  name: string;
-  mockupImage: string;
-  overview: string;
-  technologies: string[];
-  industry: string;
-  year: number;
-  images: string[];
-  github : string[];
-}
+
 @Component({
   selector: 'app-project-page',
   templateUrl: './project-page.component.html',
@@ -24,79 +15,8 @@ interface Project {
 export class ProjectPageComponent implements OnInit {
   private router = inject(ActivatedRoute);
   private magneticService = inject(MagnetizeElementsService);
+  private projectsService = inject(ProjectsDataService);
   
-  projects: Project[] = [
-    {
-      id: 'springnet',
-      name: 'SPRINGNET - SPRINGNET - SPRINGNET -',
-      mockupImage: '../../../assets/images/spring-mockup.png',
-      technologies: ['Springboot', 'Angular', 'MySQL', 'AWS'],
-      images: [
-        '../../../assets/images/springnet-gallery.png',
-      ],
-      industry: 'Social Media',
-      overview: `SpringNet allows you to create posts and stories, view, comment and like other users' posts, follow or unfollow profiles. It has a chat that works in real time using a websocket connection.
-      \n\n
-      For the development, I used Spring Boot for the backend, since I have experience in this framework and I feel comfortable working with Java. For the frontend, I decided to use Angular, a technology I've been learning and I'm excited to keep improving.`,
-      year: 2023,
-      github: ['https://github.com/AndersonGG21/SpringNet-Front', 'https://github.com/AndersonGG21/SpringNet']
-    },
-    {
-      id: 'idioom',
-      name: 'IDIOOM - IDIOOM - IDIOOM -',
-      mockupImage: '../../../assets/images/idioom-mockup.png',
-      technologies: ['Springboot', 'JavaScipt', 'MySQL'],
-      images: [
-        '../../../assets/images/idioom1.webp',
-        '../../../assets/images/idioom2.webp',
-        '../../../assets/images/idioom3.webp',
-        '../../../assets/images/idioom4.webp'
-      ],
-      industry: 'Education',
-      overview: `<p>SpringNet allows you to create posts and stories, view, comment and like other users' posts, follow or unfollow profiles. It has a chat that works in real time using a websocket connection.
-      </p>
-      <br>
-      <p>For the development, I used Spring Boot for the backend, since I have experience in this framework and I feel comfortable working with Java. For the frontend, I decided to use Angular, a technology I've been learning and I'm excited to keep improving.
-      </p>`,
-      year: 2022,
-      github: ['https://github.com/AndersonGG21/SpringNet-Front', 'https://github.com/AndersonGG21/SpringNet']
-    },
-    {
-      id: 'crm',
-      name: 'CRM - CRM - CRM -',
-      mockupImage: '../../../assets/images/spring-mockup.png',
-      technologies: ['Springboot', 'JavaScipt', 'MySQL'],
-      images: [
-        '../../../assets/images/crm1.png',
-        '../../../assets/images/crm2.png',
-        '../../../assets/images/crm3.png',
-        '../../../assets/images/crm4.png',
-      ],
-      industry: '',
-      overview: 'CRM',
-      year: 2021,
-      github: ['https://github.com/AndersonGG21/SpringNet-Front', 'https://github.com/AndersonGG21/SpringNet']
-    },
-    {
-      id: 'food',
-      name: 'Food App',
-      mockupImage: '../../../assets/images/spring-mockup.png',
-      technologies: ['Angular', 'Firebase'],
-      images: [
-        '../../../assets/images/springnet1.png',
-        '../../../assets/images/springnet2.png',
-        '../../../assets/images/springnet3.png',
-        '../../../assets/images/springnet4.png',
-        '../../../assets/images/springnet5.png',
-      ],
-      industry: 'Social Media',
-      overview: `SpringNet allows you to create posts and stories, view, comment and like other users' posts, follow or unfollow profiles. It has a chat that works in real time using a websocket connection.
-      /n
-      For the development, I used Spring Boot for the backend, since I have experience in this framework and I feel comfortable working with Java. For the frontend, I decided to use Angular, a technology I've been learning and I'm excited to keep improving.`,
-      year: 2021,
-      github: ['https://github.com/AndersonGG21/SpringNet-Front', 'https://github.com/AndersonGG21/SpringNet']
-    },
-  ];
 
   project: Project = {
     id: '',
@@ -114,7 +34,8 @@ export class ProjectPageComponent implements OnInit {
     this.router.paramMap.subscribe((params: ParamMap) => {
       let projectId = params.get('project');
 
-      let project = this.projects.find((project) => project.id == projectId);
+      let project = this.projectsService.getProjectById(projectId || '');
+      
       if (project) {
         this.project = project;
       }
---
+++ src/app/services/projects-data.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+
+export interface Project {
+  id: string;
+  name: string;
+  mockupImage: string;
+  overview: string;
+  technologies: string[];
+  industry: string;
+  year: number;
+  images: string[];
+  github : string[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectsDataService {
+
+  projects: Project[] = [
+    {
+      id: 'springnet',
+      name: 'SPRINGNET - SPRINGNET - SPRINGNET -',
+      mockupImage: '../../../assets/images/springnet-mockup.png',
+      technologies: ['Springboot', 'Angular', 'MySQL', 'AWS'],
+      images: [
+        '../../../assets/images/springnet-gallery.png',
+      ],
+      industry: 'Social Media',
+      overview: `SpringNet allows you to create posts and stories, view, comment and like other users' posts, follow or unfollow profiles. It has a chat that works in real time using a websocket connection.
+      For the development, I used Spring Boot for the backend, since I have experience in this framework and I feel comfortable working with Java. For the frontend, I decided to use Angular, a technology I've been learning and I'm excited to keep improving.`,
+      year: 2023,
+      github: ['https://github.com/AndersonGG21/SpringNet-Front', 'https://github.com/AndersonGG21/SpringNet']
+    },
+    {
+      id: 'idioom',
+      name: 'IDIOOM - IDIOOM - IDIOOM -',
+      mockupImage: '../../../assets/images/idioom-mockup.png',
+      technologies: ['Springboot', 'JavaScipt', 'MySQL'],
+      images: [
+        '../../../assets/images/idioom-gallery.png',
+      ],
+      industry: 'Education',
+      overview: 'Idioom LMS is a digital tool that enables organizations, educational institutions and companies to create, manage and deliver online courses and training content. It facilitates the efficient management of the entire learning process, from student enrollment to the evaluation of their progress. This project was the first time I used agile methodologies (Scrumban) with my team and my role was that of Scrum Master.',
+      year: 2022,
+      github: ['https://github.com/AndersonGG21/LanguageAcademyLMS']
+    },
+    {
+      id: 'crm',
+      name: 'CRM - CRM - CRM -',
+      mockupImage: '../../../assets/images/crm-mockup.png',
+      technologies: ['Springboot', 'JavaScipt', 'MySQL'],
+      images: [
+        '../../../assets/images/crm-gallery.png',
+      ],
+      industry: '',
+      overview: 'The hardware distribution company CRM (Manage Customer Relationships) in this case called JDAS is designed for the easy manipulation and organization of internal information, for example customers contacts, sales and products registers, whit de possibility of generating statistical reports, all this managed for registered users in the system. This was my first team project.',
+      year: 2021,
+      github: ['https://github.com/luisdavid24/CRM_Hardware_Industry']
+    },
+    {
+      id: 'food',
+      name: 'Food App - Food App - Food App - Food App -',
+      mockupImage: '../../../assets/images/food-mockup.png',
+      technologies: ['Angular', 'Firebase'],
+      images: [
+        '../../../assets/images/food-gallery.png',
+      ],
+      industry: 'Gastronomy',
+      overview: 'This was my first project using Angular and Firebase. I was tasked with creating a food ordering app. The app allows users to order food and add it to their cart. The app also allows users to view their cart and checkout. The app also allows users to login. The app also allows chefs to view the order that users made',
+      year: 2021,
+      github: ['https://github.com/AndersonGG21/food-app']
+    },
+  ];
+
+  getProjectById(id: string) : (Project | null) {
+    let project = this.projects.find((project) => project.id == id) || null ;
+    return project;
+  }
+  
+}
